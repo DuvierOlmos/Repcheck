29a996f01a622e7c49f7b26a444c39104323252d
DuvierOlmos <olmos.4561@gmail.com>
2025-12-12 15:04:11 -0500

    preparando archivos para merge

--- usuarios_AGRO/backend/routes/descuentos_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const descuentosController = require("../controllers/descuentos_controller");
-
-router.get("/activos", descuentosController.getDescuentosActivos);
-router.post("/validar", descuentosController.validarCodigo);
-
-module.exports = router;
