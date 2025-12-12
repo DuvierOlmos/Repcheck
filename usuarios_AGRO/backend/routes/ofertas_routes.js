23ce88c4d8146aab8b95390b687e96d51eee638b
DuvierOlmos <olmos.4561@gmail.com>
2025-12-12 16:50:37 -0500

    actualizando rama produccion

--- usuarios_AGRO/backend/routes/ofertas_routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/ofertas_controller");
-router.get("/", controller.getOfertasActivas);
-router.post("/validar-codigo", controller.validarCodigo); 
-
-module.exports = router;
