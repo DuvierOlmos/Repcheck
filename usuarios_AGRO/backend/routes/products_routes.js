13f29caf5f6575509d889913297d71da4b93da4c
DuvierOlmos <olmos.4561@gmail.com>
2025-12-12 16:58:33 -0500

    actualizando la rama testing

--- usuarios_AGRO/backend/routes/products_routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/product_controller");
-router.get("/", productController.getAllProducts);
-router.get("/:id_producto", productController.getProductById);
-
-module.exports = router;
--- usuarios_AGRO/backend/routes/review_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const reviewController = require("../controllers/review_controller");
-
-router.post("/", reviewController.createReview);
-router.get("/product/:id_producto", reviewController.getReviewsByProduct);
-router.get("/", reviewController.getAllReviews);
-
-module.exports = router;
